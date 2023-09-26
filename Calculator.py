73a7091e5bf294c7d50fbc3da57796f014b335e7
CapJordo1 <capjordo@gmail.com>
2023-09-26 18:34:47 -0400

    Update Calculator.py
    Calculator update #1 - added comments & UX

--- Calculator.py
+++ Calculator.py
@@ -1,4 +1,4 @@
-#Python Exercise - Let's Make a Calculator!#
+#Python Exercise - Let's Make a Calculator!
 
 #Defining functions for add, subtract, multiply, and divide
 def add(a, b):
@@ -10,21 +10,25 @@ def mul(a, b):
 def div(a, b):
     return a / b
 
+#Importing time module for UX
 import time
 
-#Prompts user for operator input#
+#Defining main function, and printing message w/ time delay to simulate boot-up (UX)
 def main():
     print("Calculator Starting...")
 
     time.sleep(3)
-
+#Creating loop for repeated calculations (UX)
+    #Loop start
     while True:
+        #Prompts user for operator input
         print("Select Operation:")
         print("1. Add 2. Sub 3. Mul 4. Div")
-        #Operation choice by user#
+        #Operation choice by user
         choice = input("Enter choice (1/2/3/4): ")
-        #Get user input for both numbers#
+        #Using try and except blocks to catch errors
         try:
+            #Get user input for both numbers
             num1 = float(input("Enter first number: "))
             num2 = float(input("Enter second number: "))
             if choice == '1':
@@ -46,9 +50,11 @@ def main():
         except ValueError:
             print('Invalid input. Please enter numeric values for num1 and num2.')
         another_calculation = input("Do you want to perform another calculation? (yes/no)")
+        #Accounts for both 'yes' and 'y' as acceptable inputs for running program again, but breaks loop otherwise
         if another_calculation.lower() not in ('yes', 'y'):
-            print("Terminating Session...")
+            print("\nTerminating Session...\n")
             time.sleep(3)
             break
+        #Loop end
 if __name__ == "__main__":
     main()
