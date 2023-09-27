595cc2b20f91a463e6a35497b8aba2a2ceb175d7
CapJordo1 <capjordo@gmail.com>
2023-09-27 17:40:42 -0400

    Big update
    Improved UX/UI with pauses and colored text to improve visibility

--- RPGBattle.py
+++ RPGBattle.py
@@ -1,81 +1,80 @@
-#Initialize character & enemy placeholders
 import random
-
-#Gameplay loop start
+import time
+import os
+# Gameplay loop start
 while True:
-    
-    #Initialize character & enemy placeholders
+    # Initialize character & enemy placeholders
     player_character = {
-    'Name': 'Hero', 
-    'Level': 5, 
-    'HP': 15
+        'Name': 'Tater',
+        'Level': 5,
+        'HP': 15
     }
     enemy_creature = {
-    'Name': 'Bandit', 
-    'Level': 3, 
-    'HP': 10
+        'Name': 'Bandit',
+        'Level': 3,
+        'HP': 10
     }
-    
-    #Introductory message to inform player they are about to fight
-    print('\nYou encountered a wild bandit!\n')
+    # Introductory message to inform the player they are about to fight
+    print('\n\033[95mYou encountered a wild bandit!\n\033[0m')
 
-    #Define user's available moves and their damage amounts
+    # Define user's available moves and their damage amounts
     available_moves = {
-        '1': 2,
-        '2': 0,
-        '3': 4,
-        '4': 5
+        'Sword Slash': 2,
+        'Riposte': 0,
+        'Fireball': 10,
+        'Block': 5
     }
-    
-    #Initialize enemy HP
+    # Initialize enemy HP
     enemy_hp = 10
-    
-    #Initialize player HP
+
+    # Initialize player HP
     player_hp = 15
-    
-    #Displays available moves/damage to the user
-    print('Available Moves:')
-    for move, damage in available_moves.items():
-        print(f'{move} - Damage: {damage}')
-    
+
     while True:
-        #Takes player input for choices
-        player_choice = input('\nPlease choose a move... (1/2/3/4)\n')
-        
-        #Check if user's input is valid (from available_moves)
-        if player_choice in available_moves:
-            
-            #Calculates damage value from player choice
-            damage_dealt = available_moves[player_choice]
-            print(f'You used {player_choice} and dealt {damage_dealt} damage!\n')
-            
-            #Apply damage to enemy
-            enemy_hp -= damage_dealt
-            
-            #Display the enemy's remaining HP
-            print(f'Enemy health remaining: {enemy_hp} \n')
-            if enemy_hp <= 0: #Check if enemy is defeated (HP <=0)
-                print('Congrats! You defeated the Bandit!\n')
-                break #Exit game loop as the player wins
-        else:
-            print ('Invalid move. Please select a valid move.\n')
+        # Start of inner loop. Displays available moves/damage to the user before they make a selection.
+        print('\033[94mAvailable Moves:\033[0m')
+        for i, move_name in enumerate(available_moves.keys(), start=1):
+            print(f'{i}: {move_name} - Damage: {available_moves[move_name]}')
+        try:
+            # Takes player input for choices
+            player_choice = int(input("\n\033[93mSelect a move by typing the corresponding number...\033[0m\n"))
+            if player_choice in range(1, 5):
+                # Check if user's input is valid (from available_moves)
+                selected_move_name = list(available_moves.keys())[player_choice - 1]
+                damage_dealt = available_moves[selected_move_name]
+                print(f'\nYou used {selected_move_name} and dealt \033[91m{damage_dealt}\033[0m damage!')
+                time.sleep(1.5)
 
-        #Implement the enemy's attack (bandit's turn)
-        enemy_move = random.choice(list(available_moves.keys()))
-        enemy_damage = available_moves[enemy_move]
+                # Apply damage to the enemy
+                enemy_hp -= damage_dealt
 
-        #Apply damage to the player's creature.
-        player_hp -= enemy_damage
+                # Check if enemy is defeated (HP <= 0)
+                if enemy_hp <= 0:
+                    print('Congrats! You defeated the Bandit!\n')
+                    break  # Exit the inner loop
 
-        #Display bandit's move and damage dealt
-        print(f'The wild creature used {enemy_move} and dealt {enemy_damage}')
+                # Implement the enemy's attack (bandit's turn)
+                enemy_move = random.choice(list(available_moves.keys()))
+                enemy_damage = available_moves[enemy_move]
 
-        #Check if player is defeated (HP <= 0)
-        if player_hp <= 0:
-            print('Game over!')
-            break
+                # Apply damage to the player's creature.
+                player_hp -= enemy_damage
 
-    #Asks player if they would like to play again, and breaks loop if no is selected
-    play_again = input('Do you want to play again? (y/n)')
+                # Display bandit's move and damage dealt
+                print(f'The bandit used {enemy_move} and dealt {enemy_damage}\n')
+                time.sleep(1.5)
+
+                # Check if the player is defeated (HP <= 0)
+                if player_hp <= 0:
+                    print('Game Over!')
+                    break  # Exit the inner loop
+            else:
+                print('Invalid input. Please enter a valid move (1-4).\n')
+        except ValueError:
+            print('Invalid input. Please enter a valid number.\n')
+
+    # Ask the player if they want to play again
+    play_again = input('\033[93mDo you want to play again? (y/n)\033[0m\n')
     if play_again.lower() != 'y':
-        break
+        os.system('clear' if os.name == 'posix' else 'cls')
+        break  # Exit the outer loop
